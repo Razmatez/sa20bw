b78edbfd78bfb17a3592e18391fbd44ad85b80cd
Darren Powell <darren.powell@wundermanthompson.com>
2023-08-04 16:13:16 +0200

    Wunjhbsa20-32: Add UI color scheme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,27 @@ module.exports = {
 				"gradient-conic":
 	      "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
 			},
+			colors: {
+				// Primary colors
+				darkBlue: "#0A003E",
+				lightBlue: "#00FFFF",
+				lime: "#DAFF00",
+				brightGreen: "#59FF26",
+				
+				// Secondary colors
+				black: "#000000",
+				darkGrey: "#4D4D4D",
+				darkYellow: "#DAFF00",
+				//yellow: "#ffff00"  tailwind = yellow-200-accent
+				darkGreen: "#009426",
+				green: "#00ff00",
+				grey: "#c3c3c3",
+				//lightGrey: "#f5f5f5" tailwind = grey-100
+				brightBlue: "#0000FF",
+				electricBlue: "#00a0ff",
+				red: "#be0010",
+				magenta: "#fd00be",
+			}
 		},
 	},
 	plugins: [
