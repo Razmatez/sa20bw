1264de332bb3622cb4766aba7588f2765ae8a9eb
Lucia Ma <lucia.ma@Joh7f-LuciaMa.local>
2023-08-08 12:18:07 +0200

    WUNJHBSA-20-14: Added Checkbox component with story.

--- app/components/01-atoms/checkbox/checkbox.stories.js
+++ app/components/01-atoms/checkbox/checkbox.stories.js
@@ -1,13 +1,17 @@
 /**
  * Storybook Definition.
  */
-import { Checkbox } from "./checkbox";
+import Checkbox from "./checkbox";
 
 export default {
 	title: "Atoms/Checkbox",
 	component: Checkbox,
-	tags: ["docsPage"],
 	argTypes: {},
 };
 
-export const checkbox = () => <Checkbox key={1} checked={true} label={"Some label"}/>;
+export const Primary = {
+	args: {
+		id: "",
+		label: "Checkbox label",
+	},
+};
