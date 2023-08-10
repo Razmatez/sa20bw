29fcde362b7d0a85e91f5339273c00e05f902ea1
Lucia Ma <lucia.ma@Joh7f-LuciaMa.local>
2023-08-10 18:55:59 +0200

    WUNJHBSA20-25: Updated Table component to take in dynamic content and stories.

--- app/components/01-atoms/table/table.stories.js
+++ app/components/01-atoms/table/table.stories.js
@@ -1,6 +1,14 @@
-/**
- * Storybook Definition.
- */
-export default { title: "Atoms/Table" };
+import Table from "./table";
 
-export const table = () => {};
+export default {
+	title: "Atoms/Table",
+	component: Table,
+	argTypes: {},
+};
+
+export const Primary = {
+	args: {
+		contentHeadings: ["Heading 1", "Heading 2", "Heading 3"],
+		content: [["Cell 1", "Cell 2", "Cell 3"], ["Cell 1", "Cell 2", "Cell 3"]]
+	},
+};
