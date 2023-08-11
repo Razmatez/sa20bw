c7ff23dcaa77fb3c212acccb31069eb7d9cc0e15
Lucia Ma <lucia.ma@Joh7f-LuciaMa.local>
2023-08-11 16:30:54 +0200

    WUNJHBSA20-22: Added basic Pagination component as per wireframes.

--- app/components/01-atoms/pagination/pagination.js
+++ app/components/01-atoms/pagination/pagination.js
@@ -1,10 +1,23 @@
 "use client";
+import React, { useState } from "react";
+import { Pagination as PaginationComponent } from "flowbite-react";
 
 const Pagination = (props) => {
+
+	const [currentPage, setCurrentPage] = useState(1);
+
 	return (
-		<div>
-		</div>
+
+		<PaginationComponent
+			currentPage={currentPage}
+			layout="navigation"
+			onPageChange={page => setCurrentPage(page)}
+			showIcons
+			totalPages={100}
+	  />
+
 	)
+
 }
 
 export default Pagination;
