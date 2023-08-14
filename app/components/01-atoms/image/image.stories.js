33ab4c4d070ab829357018f5c68b7ab00a6f1887
Darren Powell <darren.powell@wundermanthompson.com>
2023-08-14 10:55:03 +0200

    Wunjhbsa20-18: Add atom for image.

--- app/components/01-atoms/image/image.stories.js
+++ app/components/01-atoms/image/image.stories.js
@@ -1,15 +1,22 @@
-/**
- * Storybook Definition.
- */
-import { Image } from "./image";
+import Image from './image';
 
 export default {
-	title: "Atoms/Responsive Image",
+	title: 'Atoms/Image',
 	component: Image,
-	tags: ["docsPage"],
-	argTypes: {},
 };
 
-export const image = () => (
-	<Image />
-)
+const Template = (args) => <Image {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+	image: "https://flowbite.com/docs/images/examples/image-1@2x.jpg",
+	style: "h-auto max-w-full",
+	altText: "alt text"
+};
+
+export const roundImage = Template.bind({});
+roundImage.args = {
+	image: "https://flowbite.com/docs/images/examples/image-1@2x.jpg",
+	style: "rounded-full w-96 h-96",
+	altText: "alt text"
+};
