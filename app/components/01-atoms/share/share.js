b17aed06a33a297f823d0551777eb51a4bb1c987
Darren Powell <darren.powell@wundermanthompson.com>
2023-08-14 13:54:03 +0200

    Wunjhbsa20-24: Create social share atom.

--- app/components/01-atoms/share/share.js
+++ app/components/01-atoms/share/share.js
@@ -1,8 +1,32 @@
+import Image from "next/image";
 
-export const Share = () => {
+const Share = (props) => {
+
+	const { items } = props;
 
 	return (
-		<div></div>
-	)
+		<div className="flex items-center">
+
+			{items.map((item, index) => (
+				<a href={item.href}>
+					<Image
+						key={index}
+						src={item.src}
+						alt={item.altText}
+						href={item.href}
+						width={30}
+						height={30}
+					>
+
+					</Image>
+				</a>
+
+			))}
+
+		</div>
+
+	);
 
 }
+
+export default Share;
