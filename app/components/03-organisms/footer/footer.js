1563b95c4e9172b1089c20b939d39934d5532ac2
Lucia Ma <lucia.ma@Joh7f-LuciaMa.local>
2023-08-18 15:45:00 +0200

    WUNJHBSA20-55: Fixed issue with atom component change.

--- app/components/03-organisms/footer/footer.js
+++ app/components/03-organisms/footer/footer.js
@@ -1,58 +1,56 @@
+import Link from "next/link";
+
 import SocialMenu from "../../02-molecules/social-menu/social-menu";
 import Subscribe from "../../02-molecules/subscribe/subscribe";
 import FooterMenu from "../../02-molecules/footer-menu/footer-menu";
 import Divider from "../../01-atoms/divider/divider";
 import Text from "../../01-atoms/text/text";
-import Link from "../../01-atoms/link/link"
-
-
-
 
 const Footer = () => {
-    return (
-        <div className="bg-darkBlue">
+	return (
+		<div className="bg-darkBlue">
 
-            <div className="flex">
+			<div className="flex">
 
-                <div className="w-[25%]">
+				<div className="w-[25%]">
 
-                     <Subscribe />
+					<Subscribe />
 
-                     <SocialMenu />
+					<SocialMenu />
 
-                 </div>
+				</div>
 
-            <div className="w-[75%]">
+				<div className="w-[75%]">
 
-                <FooterMenu />
+					<FooterMenu />
 
-            </div>
+				</div>
 
-        </div>
+			</div>
 
-            <Divider darkTheme />
+			<Divider darkTheme />
 
-        <div className="text-white flex pt-8 pb-8 pl-8 pr-24">
+			<div className="text-white flex pt-8 pb-8 pl-8 pr-24">
 
-            <div className="w-[25%]">
+				<div className="w-[25%]">
 
-                <Text content="© Copyright SA20 2023. All Rights Reserved." />
+					<Text content="© Copyright SA20 2023. All Rights Reserved." />
 
-            </div>
+				</div>
 
-                <div className="w-[75%] flex justify-between">
+				<div className="w-[75%] flex justify-between">
 
-                    <Link label="Tickets Terms & Conditions" />
-                    <Link label="Priavcy Policy" />
-                    <Link label="Terms of Use" />
-                    <Link label="Cookie Policy" />
+					<Link href="/">Tickets Terms & Conditions</Link>
+					<Link href="/">Priavcy Policy</Link>
+					<Link href="/">Terms of Use</Link>
+					<Link href="/">Cookie Policy</Link>
 
-                </div>
+				</div>
 
-            </div>
+			</div>
 
-        </div>
-    )
+		</div>
+	)
 }
 
 export default Footer;
