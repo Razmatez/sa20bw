0d5ad1f33262094530d45e1a908cf5919cb624f3
Lucia Ma <lucia.ma@Joh7f-LuciaMa.local>
2023-08-23 15:27:32 +0200

    WUNJHBSA20-75: Added HeroSlide molecule component with placeholder image.

---
+++ components/02-molecules/hero-slide/hero-slide.js
@@ -0,0 +1,19 @@
+
+const HeroSlide = () => {
+
+	return (
+
+		<div className="h-[640px]">
+
+			<img
+				className="w-full h-full object-cover"
+				src="https://media-cdn.incrowdsports.com/0663bed6-91d3-40b6-9f1e-935bc6379dcb.png"
+			/>
+
+		</div>
+
+	)
+
+}
+
+export default HeroSlide;
