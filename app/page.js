6ec8a841c8606a11141c669c2342d571d70f012e
Darren Powell <darren.powell@wundermanthompson.com>
2023-08-24 18:12:04 +0200

    Fix pr.

--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
 "use client";
 
-import CtaBanner from "../components/03-organisms/cta-banner/cta-banner";
-
 export default function Home() {
 	return (
 		<main>
-			<CtaBanner />
+			
 		</main>
 	)
 }
