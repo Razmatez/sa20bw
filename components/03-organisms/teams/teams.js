4bb1a02e28cd58fb926f94b9ff4e63943c41b212
Darren Powell <darren.powell@wundermanthompson.com>
2023-09-04 13:19:27 +0200

    PR updates.

--- components/03-organisms/teams/teams.js
+++ components/03-organisms/teams/teams.js
@@ -48,9 +48,9 @@ const Teams = (props) => {
     ];
 
     return (
-        <div className="p-5 md:p-16">
+        <div className="pt-6 pb-10 px-5 md:pt-8 pb-16 px-16">
 
-            <h1 className="pb-8">Teams</h1>
+            <h1 className="py-4 md:py-8">Teams</h1>
 
             <div className="grid grid-cols-3 md:grid-cols-6 gap-4">
 
