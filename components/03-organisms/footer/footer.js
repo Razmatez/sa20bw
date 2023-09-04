5e935c4ce20b6dbf6ab756dfc43e0138b8c1aa6d
Aluwani Raswiswi <aluwani.raswiswi@wundermanthompson.com>
2023-09-04 21:45:19 +0200

    chore: remove usused imports

--- components/03-organisms/footer/footer.js
+++ components/03-organisms/footer/footer.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState, useEffect } from "react";
 
 import Logo from "../../../assets/icons/logo.svg";
 import Overlay from "../../../assets/icons/footer-overlay.svg"
