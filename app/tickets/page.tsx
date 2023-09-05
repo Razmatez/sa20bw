70bf18464adb24d61c447ac27a14ffee8a879f5d
Aluwani Raswiswi <aluwani.raswiswi@wundermanthompson.com>
2023-09-05 15:29:34 +0200

    style: make it full width and height

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
     return (
-        <div>
-            <iframe src={process.env.NEXT_PUBLIC_TICKETPROS_IFRAME} frameBorder="0"></iframe>
+        <div className="flex flex-col overflow-hidden height-full w-full">
+            <iframe src={process.env.NEXT_PUBLIC_TICKETPROS_IFRAME} className="grow border-0"></iframe>
         </div>
     )
 }
