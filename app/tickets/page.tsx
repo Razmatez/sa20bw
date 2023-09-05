31453923f5158ee1a7b68bde3a6481710f072101
Aluwani Raswiswi <aluwani.raswiswi@wundermanthompson.com>
2023-09-05 15:06:55 +0200

    feat: integrate ticket pros iframe

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,8 +1,7 @@
-
 export default function Page() {
     return (
         <div>
-            <iframe src="https://www.ticketpros.co.za/portal/web/index.php/parent_event/49b2dfe0-df0b-3cee-af55-6464792c8b1d" frameBorder="0"></iframe>
+            <iframe src={process.env.NEXT_PUBLIC_TICKETPROS_IFRAME} frameBorder="0"></iframe>
         </div>
     )
 }
