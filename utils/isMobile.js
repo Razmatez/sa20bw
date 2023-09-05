bba5c8904fe7b7aa97a4b7194a648c9ed99d05d5
Lucia Ma <lucia.ma@Joh7f-LuciaMa.local>
2023-09-05 17:35:18 +0200

    PR fixes.

--- utils/isMobile.js
+++
@@ -1,9 +0,0 @@
-const isMobile = () => {
-
-	const windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-
-	return windowWidth < 1024;
-
-}
-
-export default isMobile;
