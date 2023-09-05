3d9ce4a7010ea960147e2f3edd8dc7450b7f4624
Darren Powell <darren.powell@wundermanthompson.com>
2023-09-05 13:14:53 +0200

    Wunjhbsa20- PR updates.

--- components/03-organisms/quick-links/quick-links.js
+++ components/03-organisms/quick-links/quick-links.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React, { useState } from "react";
 
 import Ticket from "../../../assets/icons/quick-links/Ticket.svg";
@@ -35,22 +36,19 @@ const QuickLinks = (props) => {
     };
 
     return (
-        <div className="px-5 md:px-16">
-            <h3 className="pb-6 md:pb-8">What are you looking for ?</h3>
+        <div className="px-5 lg:px-16">
+            <h3 className="pb-6 lg:pb-8">What are you looking for ?</h3>
             <div className="pb-10 flex flex-wrap gap-4">
                 {links.map((link, index) => (
-                    <a key={index} href={link.url} className="bg-darkBlue text-white body-sm md:body-xl md:text-white p-4 md:p-6 rounded-lg w-[167px] h-[100px] md:w-[310px] md:h-[104px] mb-4 flex items-center">
-                        <div
+                    <Link key={index} href={link.url} className="bg-darkBlue text-white body-sm lg:body-xl lg:text-white p-4 lg:p-6 rounded-lg w-[167px] h-[100px] lg:w-[310px] lg:h-[104px] mb-4 flex items-center">
+                        <div className="flex items-start lg:items-center flex-col lg:flex-row lg:hover:scale-110 transform transition-transform duration-500 ease-in-out w-full"
                             onMouseEnter={() => handleMouseEnter(index)}
-                            onMouseLeave={() => handleMouseLeave(index)}
-                        >
-                            <div className="flex items-start lg:items-center flex-col lg:flex-row md:hover:scale-110 transform transition-transform duration-500 ease-in-out w-full">
-                                <Image src={hoveredStates[index] ? link.hovered : link.icon} alt={link.text} className="mr-3"
-                                />
-                                {link.text}
-                            </div>
+                            onMouseLeave={() => handleMouseLeave(index)}>
+                            <Image src={hoveredStates[index] ? link.hovered : link.icon} alt={link.text} className="mr-3"
+                            />
+                            {link.text}
                         </div>
-                    </a>
+                    </Link>
                 ))}
             </div>
         </div>
