4092a4ec5dafe42b99b368fb5deddd7a4c06eb3f
Darren Powell <darren.powell@wundermanthompson.com>
2023-09-07 08:40:24 +0200

    Wunjhb-Style updates and matches page creation.

---
+++ app/matches/page.js
@@ -0,0 +1,10 @@
+'use client'
+
+export default async function Page({ params }) {
+
+    return (
+        <div className='mx-auto flex flex-wrap items-center justify-between'>
+            hello
+        </div>
+    );
+}
